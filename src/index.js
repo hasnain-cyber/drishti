7cb9027996fbbad5ee59fd4533ac7568d8712d4d
Hasnain Raza <hasnainraza1072@gmail.com>
2021-10-23 23:35:15 +0530

    fixed the strict mode

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
     <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+        <App/>
+    </BrowserRouter>,
+    document.getElementById("root")
 );
