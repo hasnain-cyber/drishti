6db2e68703da6a2e4dccb6afefcf770f48e9d9e7
Hasnain Raza <hasnainraza1072@gmail.com>
2021-10-31 01:33:44 +0530

    added a secondary navbar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { Route, Switch } from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
 import GoverningBody from "./pages/Organisation/GoverningBody";
 import Navbar from "./components/Navbar/Navbar";
 import BoardOfDirectors from "./pages/Organisation/BoardOfDirectors";
 import ExecBody from "./pages/Organisation/Exec_Body";
 import ImplementationCommittees from "./pages/Organisation/Implementation_committees";
 import MainFooter from "./components/MainFooter/MainFooter";
-import Box from "@mui/material/Box";
 import React from "react";
 
 let theme = createTheme({
@@ -28,17 +27,17 @@ let theme = createTheme({
 function App() {
     return (
         <ThemeProvider theme={theme}>
-            <Navbar />
+            <Navbar/>
 
             <Switch>
-                <Route exact path="/" component={LandingPage} />
-                <Route exact path="/gov_body" component={GoverningBody} />
-                <Route exact path="/bod" component={BoardOfDirectors} />
-                <Route exact path="/exec_body" component={ExecBody} />
-                <Route exact path="/implementation_committees" component={ImplementationCommittees} />
+                <Route exact path="/" component={LandingPage}/>
+                <Route exact path="/gov_body" component={GoverningBody}/>
+                <Route exact path="/bod" component={BoardOfDirectors}/>
+                <Route exact path="/exec_body" component={ExecBody}/>
+                <Route exact path="/implementation_committees" component={ImplementationCommittees}/>
             </Switch>
             {/*box to provide the margin to the top of footer*/}
-            <MainFooter />
+            <MainFooter/>
         </ThemeProvider>
     );
 }
